a830fbd27d39136f9d2e0a54989ce7057d207516
Josh Sjah <luckystripes60@gmail.com>
2024-09-11 00:12:27 -0700

    Update bot.py
    2024 edition

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ import json
 import os
 from dotenv import load_dotenv
 
-
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
@@ -29,7 +28,10 @@ async def list_people(message):
 async def direct_message(person, match, message):
     member = discord.utils.get(message.guild.members, name=person)
     channel = await member.create_dm()
+    #embed=discord.Embed(title="test", description='{}, test'.format(user.mention) , color=0xecce8b)
+    #embed.set_image(url=(pfp))
     await channel.send("your secret santa recipient is: " + match)
+    #await client.send_message(message.channel, embed=embed)
 
 async def save_names(message, pairs):
     dictionaries[message.guild.name] = pairs
@@ -47,12 +49,25 @@ async def read_json(message):
     except:
         return
 
+def shuffle(x):
+    copy = x.copy()
+    if len(x) == 1:
+        raise Exception
+    random.shuffle(x)
+    for i in range(len(x)):
+        if x[i] == copy[i]:
+            print(i)
+            r = list(range(0,i)) + list(range(i+1, len(x)))
+            print(r)
+            index = r[random.randint(0, len(r)-1)]
+            print(index)
+            x[index], x[i] = x[i], x[index]
+
 @client.event
 async def on_message(message):
     # don't respond to ourselves
     if message.author.bot:
         return
-
 
     elif message.content == '!uses':
         await message.channel.send("usages: !umu, !people, !me, !roll")
@@ -82,18 +97,14 @@ async def on_message(message):
             await message.channel.send("Already distributed names.")
             return
         member_list = await list_people(message)
-        recipient_list = await list_people(message)
-        for people in member_list:
-            match = people
-            while(match == people):
-                index = random.randint(0, len(recipient_list)-1)
-                match = recipient_list[index]
-            recipient_list.remove(match)
+        recipient_list = member_list.copy()
+        shuffle(recipient_list)
+        for i in range(len(recipient_list)):
             try:
-                await direct_message(people, match, message)
+                await direct_message(member_list[i], recipient_list[i], message)
             except:
-                await message.channel.send(f"{people} cannot receieve a message") 
-            pairs[people] = match
+                await message.channel.send(f"{member_list[i]} cannot receieve a message")
+            pairs[member_list[i]] = recipient_list[i]
         await message.channel.send("finished distributing names")
         await save_names(message, pairs)
 
